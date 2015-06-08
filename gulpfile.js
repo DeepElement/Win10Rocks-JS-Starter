b85fd39fd5e50065c304d2b03edbcaed7668e73c
Todd Morrison <todd@deepelement.com>
2015-06-08 08:19:05 -0700

    Added win10 build versioning

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp'),
     msbuild = require("gulp-msbuild"),
-    mocha = require("gulp-mocha");
+    mocha = require("gulp-mocha"),
+    replace = require('gulp-replace');
+
+var parseEnv = function () {
+    return {
+        version:
+        {
+            major: 0,
+            minor: 0,
+            revision: 0,
+            build: 1
+        }
+    };
+};
 
 gulp.task('default', function () {
     // place code for your default task here
 });
 
-gulp.task("build-win10", function () {
+gulp.task("build-windows-10", ["build-windows-version"], function () {
     return gulp.src("./Win10Rocks-JS-Starter.sln")
         .pipe(msbuild({
         targets: ['Clean', 'Build'],
@@ -15,6 +28,17 @@ gulp.task("build-win10", function () {
             AppxBundle: 'Always'
         }
     }));
+});
+
+gulp.task("build-windows-version", function () {
+    var args = parseEnv();
+    var buildNum = args.version.major + "." 
+            + args.version.minor + "." 
+            + args.version.revision + "."
+            + args.version.build;
+    return gulp.src('./src/app-universal/package.appxmanifest', { base: './' })
+        .pipe(replace(/\sVersion=\"[^\"]+\"\s/, ' Version=\"' + buildNum + '\" '))
+        .pipe(gulp.dest('./'));
 });
 
 gulp.task("test", function () {
