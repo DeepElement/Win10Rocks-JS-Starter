0dc0fee5a081ac39cbdcc95553436e702cfcf7a7
Todd Morrison <todd@deepelement.com>
2015-06-15 08:45:59 -0700

    added loki serialization based on service life-cycle

--- src/common/sdk/service/data.node.js
+++ src/common/sdk/service/data.node.js
@@ -8,7 +8,7 @@ var main = require('../main.node'),
     feedSourceModel = require('../model/feed-source.node'),
     eventModel = require('../model/event.node');
 
-var _constructor = function(options) {
+var _constructor = function (options) {
     this._lokiDBKey = "bdf6a7de-f243-4d42-8e91-e5c974b53051";
     this._hydrating = false;
     this._hydrationInterval = 5000;
@@ -16,36 +16,31 @@ var _constructor = function(options) {
 };
 
 var members = {
-    load: function(done) {
+    load: function (done) {
         var that = this;
         that._catalogueProvider = ioc.get("catalogueProvider");
         that._lokiCatalogueProvider = ioc.get("lokiCatalogueProvider");
-        base.prototype.load.call(this, function() {
+        base.prototype.load.call(this, function () {
             async.waterfall([
-                    function(cb) {
-                        that._lokiAdapter = ioc.get("lokiStorageProvider");
-                        that._db = new loki(that._lokiDBKey, {
-                            adapter: that._lokiAdapter
-                        });
-                        that._db.loadDatabase(require('../model/mapping.node'),
-                            function(resp) {
-                                if (resp === 'Database not found')
-                                    that._db.saveDatabase();
-                                return cb();
-                            });
-                    },
-                    function(cb) {
-                        // Run the first iteration of data hydration
-                        that._lokiCatalogueProvider.build(that._db, cb);
-                    }
-                ],
-                function(err) {
+                function (cb) {
+                    that._lokiAdapter = ioc.get("lokiStorageProvider");
+                    that._db = new loki(that._lokiDBKey, {
+                        adapter: that._lokiAdapter
+                    });
+                    that._loadDatabase(cb);
+                },
+                function (cb) {
+                    // Run the first iteration of data hydration
+                    that._lokiCatalogueProvider.build(that._db, cb);
+                }
+            ],
+                function (err) {
                     if (err)
                         return done(err);
 
                     // schedule the hydrator
-                    that._hydrationTimer = that.setInterval(function() {
-                        that._hydrate(false, function() {});
+                    that._hydrationTimer = that.setInterval(function () {
+                        that._hydrate(false, function () { });
                     }, 5000);
 
                     return done();
@@ -53,12 +48,22 @@ var members = {
         });
     },
 
-    _hydrate: function(isStartup, callback) {
+    _loadDatabase: function (callback) {
+        var that = this;
+        that._db.loadDatabase(require('../model/mapping.node'),
+            function (resp) {
+                if (resp === 'Database not found')
+                    that._db.saveDatabase();
+                return callback();
+            });
+    },
+
+    _hydrate: function (isStartup, callback) {
         var that = this;
         if (!that._hydrating) {
             that._hydrating = true;
             that._lokiCatalogueProvider.update(that._db,
-                function(err) {
+                function (err) {
                     that._hydrating = false;
                     return callback(err);
                 });
@@ -67,36 +72,52 @@ var members = {
     },
 
     contributorCollection: {
-        get: function() {
+        get: function () {
             return this._db.getCollection("contributors");
         }
     },
 
     eventCollection: {
-        get: function() {
+        get: function () {
             return this._db.getCollection("events");
         }
     },
 
     mediaCollection: {
-        get: function() {
+        get: function () {
             return this._db.getCollection("media");
         }
     },
 
     feedSourceCollection: {
-        get: function() {
+        get: function () {
             return this._db.getCollection("feedSources");
         }
     },
 
-    unload: function(done) {
+    pause: function (done) {
         var that = this;
-        if (that._hydrationTimer)
-            that.clearTimeout(that._hydrationTimer);
-        if (that._db)
-            that._db.saveDatabase();
-        return done();
+        base.prototype.pause.call(this, function () {
+            that._db.saveDatabase(done);
+        });
+    },
+
+    resume: function (done) {
+        var that = this;
+        base.prototype.resume.call(this, function () {
+            that._loadDatabase(done);
+        });
+    },
+
+    unload: function (done) {
+        var that = this;
+        base.prototype.unload.call(this, function () {
+            if (that._hydrationTimer)
+                that.clearTimeout(that._hydrationTimer);
+            if (that._db)
+                that._db.saveDatabase();
+            return done();
+        });
     }
 };
 
